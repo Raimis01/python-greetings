3e333d026a6bd394eccf5b8161600b2704e33b57
Raimis01 <50775959+Raimis01@users.noreply.github.com>
2024-05-02 21:19:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import sys
 
 app = Flask(__name__)
 
@@ -7,4 +8,10 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+    # Default port
+    port = 3000
+    # Check if a port number is provided as a command line argument
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    # Run the app on the specified port
+    app.run(host="0.0.0.0", debug=True, port=port)
